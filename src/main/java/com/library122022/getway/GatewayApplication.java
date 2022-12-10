1f5a5116f93b971b949c0e058b20d4029e8ed046
Paulina Niedzialek <paulina.niedzialek@gmail.com>
2022-12-10 12:52:14 +0100

    Clean Spring Boot 3.0

---
+++ src/main/java/com/library122022/getway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.library122022.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library122022/getway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library122022.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
